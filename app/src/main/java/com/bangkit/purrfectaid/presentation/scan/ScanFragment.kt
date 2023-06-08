e23be51e0375b67b54e15efe0e14b561c433c24f
Yosua Haloho <72293596+YosuaPWH@users.noreply.github.com>
2023-06-08 23:48:42 +0700

    feat: camera and gallery

--- app/src/main/java/com/bangkit/purrfectaid/presentation/scan/ScanFragment.kt
+++ app/src/main/java/com/bangkit/purrfectaid/presentation/scan/ScanFragment.kt
@@ -1,26 +1,105 @@
 package com.bangkit.purrfectaid.presentation.scan
 
+import android.Manifest
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.bangkit.purrfectaid.R
 import com.bangkit.purrfectaid.databinding.FragmentScanBinding
+import com.bangkit.purrfectaid.utils.uriToFile
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
 
 class ScanFragment : Fragment() {
 
     private var _binding: FragmentScanBinding? = null
     private val binding get() = _binding!!
+    private lateinit var imageCapture: ImageCapture
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentScanBinding.inflate(inflater, container, false)
-
+        requestPermissionCamera()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGallery.setOnClickListener {
+            launchGallery()
+        }
+    }
+
+    private fun launchGallery() {
+        pickPhotoFromGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val pickPhotoFromGallery =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                val file = uriToFile(uri, requireActivity())
+
+//                imageFile = file
+            }
+        }
+
+    private val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            startCamera()
+        } else {
+            Toast.makeText(requireContext(), "Camera permission not granted", Toast.LENGTH_SHORT).show()
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun requestPermissionCamera() {
+        requestPermission.launch(Manifest.permission.CAMERA)
+    }
+
+    private fun startCamera() {
+        val processCameraProvider = ProcessCameraProvider.getInstance(requireContext())
+
+        processCameraProvider.addListener({
+            val cameraProvider: ProcessCameraProvider = processCameraProvider.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.pvScan.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    viewLifecycleOwner,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (e: Exception) {
+                Log.e("SCANFRAGMENT", "Gagal: $e")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
 }
---
+++ app/src/main/java/com/bangkit/purrfectaid/utils/Util.kt
@@ -0,0 +1,44 @@
+package com.bangkit.purrfectaid.utils
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+/**
+ * Created by Yosua on 08/06/2023
+ */
+private const val FILENAME_FORMAT = "dd-MMM-yyyy"
+
+val timeStamp: String = SimpleDateFormat(
+    FILENAME_FORMAT,
+    Locale.US
+).format(System.currentTimeMillis())
+
+fun uriToFile(selectedImage: Uri, context: Context): File {
+    val contentResolver = context.contentResolver as ContentResolver
+    val myFile = createCustomTempFile(context)
+
+    val inputStream = contentResolver.openInputStream(selectedImage) as InputStream
+    val outputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) {
+        outputStream.write(buf, 0, len)
+    }
+
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
+}
+
+fun createCustomTempFile(context: Context): File {
+    val storageDirectory = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDirectory)
+}
