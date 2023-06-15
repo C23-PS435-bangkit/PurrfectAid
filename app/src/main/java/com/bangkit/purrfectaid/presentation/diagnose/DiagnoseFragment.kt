6a1a8d3308fb79c2ca83a32d0b5861248ced9b6c
Yosua Haloho <72293596+YosuaPWH@users.noreply.github.com>
2023-06-15 16:30:06 +0700

    feat: Diagnose Bottom Sheet and icon auth small add feature

--- app/src/main/java/com/bangkit/purrfectaid/presentation/diagnose/DiagnoseFragment.kt
+++ app/src/main/java/com/bangkit/purrfectaid/presentation/diagnose/DiagnoseFragment.kt
@@ -5,18 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.bangkit.purrfectaid.databinding.FragmentDiagnoseBinding
+import com.bangkit.purrfectaid.presentation.scan.ScanFragment
 
 class DiagnoseFragment : Fragment() {
 
     private var _binding: FragmentDiagnoseBinding? = null
     private val binding get() = _binding!!
+    private val args: DiagnoseFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDiagnoseBinding.inflate(inflater, container, false)
+
+
 
         return binding.root
     }
--- app/src/main/java/com/bangkit/purrfectaid/presentation/scan/ScanFragment.kt
+++ app/src/main/java/com/bangkit/purrfectaid/presentation/scan/ScanFragment.kt
@@ -1,6 +1,7 @@
 package com.bangkit.purrfectaid.presentation.scan
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -17,11 +19,16 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bangkit.purrfectaid.R
+import com.bangkit.purrfectaid.databinding.DiagnoseSheetLayoutBinding
 import com.bangkit.purrfectaid.databinding.FragmentScanBinding
+import com.bangkit.purrfectaid.presentation.diagnose.DiagnoseBottomSheet
 import com.bangkit.purrfectaid.utils.Result
 import com.bangkit.purrfectaid.utils.createFile
 import com.bangkit.purrfectaid.utils.toImageMultiPart
 import com.bangkit.purrfectaid.utils.uriToFile
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 import okhttp3.MultipartBody
 
@@ -95,11 +102,25 @@ class ScanFragment : Fragment() {
             }
         }
 
+    @SuppressLint("InflateParams")
     private fun predict(image: MultipartBody.Part) {
         viewModel.predict(image).observe(viewLifecycleOwner) {
             when (it) {
                 is Result.Success -> {
                     Log.d("BERHASIL", it.data.toString())
+//                    val toDiagnose = ScanFragmentDirections.actionScanFragmentToDiagnoseFragment(it.data)
+//                    findNavController().navigate(toDiagnose)
+
+//                    val bottomSheetDialog = BottomSheetDialog()
+//
+//                    val diagnoseTitle = diagnoseSheet.bin
+                    val diagnoseSheet = DiagnoseBottomSheet()
+                    val bundle = Bundle()
+                    bundle.putString("diagnose", it.data.diagnose)
+                    bundle.putString("treatment", it.data.treatment)
+                    diagnoseSheet.arguments = bundle
+                    diagnoseSheet.show(childFragmentManager, "Expanded")
+
                 }
 
                 is Result.Loading -> {
